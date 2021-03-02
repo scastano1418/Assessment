f0292a8cbd089908d9dde6fcc0d9fbc436fe14ef
Steven Castano <s210717@students.aie.edu.au>
2021-03-02 16:40:05 +1100

    tue ass

--- AIE_Assessment_Exercise_06/Program.cs
+++ AIE_Assessment_Exercise_06/Program.cs
@@ -1,16 +1,42 @@
-﻿/*
- * Appendix 3 - Exercise 1: Alphabetize a File
- */
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace AIE_Assessment_Exercise_06
 {
     class Program
     {
+        static List<string> words = new List<string>();
+
         static void Main(string[] args)
         {
-            
+            LoadFile("./wordlist/words.txt");
+
+            Print();
+        }
+
+        static void LoadFile(string filename)
+        {
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    words.Add(line);
+                }
+            }
+        }
+
+        static void Print()
+        {
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
+
+
+
 }
